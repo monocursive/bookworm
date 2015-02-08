bc50c939442abe522024721916a56855600680a2
Maz-dev <contact@maz-dev.cc>
2015-02-08 18:27:29 +0100

    First commit

---
+++ client/layout/editor.js
@@ -0,0 +1,11 @@
+Template.editor.rendered = function() {
+  $("#main-editor").markdown({
+    autofocus:false,
+    savable:false,
+    height: "800px"
+  });
+}
+
+Template.editor.events({
+  
+});
